a96a93d308a74b437263f61d880565d912f6ed96
Cookie Engineer <cookiengineer@protonmail.ch>
2017-01-23 17:01:14 +0100

    :sparkles: Initial Flappy ENN sketch

---
+++ projects/flappy-enn/source/entity/Plane.js
@@ -0,0 +1,119 @@
+
+lychee.define('game.entity.Plane').includes([
+	'lychee.app.Sprite'
+]).exports(function(lychee, global, attachments) {
+
+	const _Sprite  = lychee.import('lychee.app.Sprite');
+	const _CONFIG  = attachments["json"].buffer;
+	const _TEXTURE = attachments["png"];
+
+
+
+	/*
+	 * IMPLEMENTATION
+	 */
+
+	let Composite = function(data) {
+
+		let settings = Object.assign({}, _CONFIG, data);
+
+
+		this.alive = true;
+
+
+		settings.texture = _TEXTURE;
+		settings.position = {
+			x: 0,
+			y: 0
+		};
+		settings.velocity = {
+			x: 0,
+			y: 0
+		};
+
+
+		_Sprite.call(this, settings);
+
+		settings = null;
+
+	};
+
+
+	Composite.prototype = {
+
+		/*
+		 * ENTITY API
+		 */
+
+		serialize: function() {
+
+			let data = _Sprite.prototype.serialize.call(this);
+			data['constructor'] = 'game.entity.Plane';
+
+
+			return data;
+
+		},
+
+
+
+		/*
+		 * CUSTOM API
+		 */
+
+		flap: function() {
+
+			this.velocity.y = -256;
+
+		},
+
+		update: function(clock, delta) {
+
+			if (this.alive === true) {
+
+				_Sprite.prototype.update.call(this, clock, delta);
+
+				this.velocity.y += (delta / 1000) * 256 * 3;
+
+			} else {
+
+				let effects = this.effects;
+				for (let e = 0, el = effects.length; e < el; e++) {
+
+					let effect = effects[e];
+					if (effect.update(this, clock, delta) === false) {
+						this.removeEffect(effect);
+						el--;
+						e--;
+					}
+
+				}
+
+			}
+
+		},
+
+		render: function(renderer, offsetX, offsetY) {
+
+			if (this.alive === true) {
+
+				_Sprite.prototype.render.call(this, renderer, offsetX, offsetY);
+
+			} else {
+
+				let effects = this.effects;
+				for (let e = 0, el = effects.length; e < el; e++) {
+					effects[e].render(renderer, offsetX, offsetY);
+				}
+
+			}
+
+		}
+
+	};
+
+
+	return Composite;
+
+});
+
---
+++ projects/flappy-enn/source/state/Game.js
@@ -0,0 +1,308 @@
+
+lychee.define('game.state.Game').requires([
+	'lychee.ai.Layer',
+	'lychee.math.Mersenne',
+	'lychee.ui.entity.Label',
+//	'game.ai.Agent',
+	'game.effect.Explosion',
+	'game.entity.Goal',
+	'game.entity.Plane',
+	'game.ui.sprite.Background'
+]).includes([
+	'lychee.app.State'
+]).exports(function(lychee, global, attachments) {
+
+	const _Agent     = lychee.import('game.ai.Agent');
+	const _Explosion = lychee.import('game.effect.Explosion');
+	const _Goal      = lychee.import('game.entity.Goal');
+	const _Mersenne  = lychee.import('lychee.math.Mersenne');
+	const _Plane     = lychee.import('game.entity.Plane');
+	const _State     = lychee.import('lychee.app.State');
+	const _BLOB      = attachments["json"].buffer;
+
+
+
+	/*
+	 * HELPERS
+	 */
+
+	const _reset_goals = function() {
+
+		let goals   = this.__cache.goals;
+		let twister = this.twister;
+		let width   = this.renderer.width;
+		let height  = this.renderer.height;
+
+		let offsetx =  1 / 2 * width;
+		let offsety = -1 / 2 * height + 128;
+
+		for (let g = 0, gl = goals.length; g < gl; g++) {
+			offsetx = Math.max(offsetx, goals[g].position.x);
+		}
+
+
+		for (let g = 0, gl = goals.length; g < gl; g++) {
+
+			let goal = goals[g];
+			if (goal.position.x < -1 / 2 * width) {
+				goal.position.x = offsetx;
+				goal.position.y = offsety + (twister.random() * (height - 256)) | 0;
+				offsetx += 256;
+			}
+
+		}
+
+	};
+
+	const _reset_planes = function() {
+
+		let planes = this.__cache.planes;
+		let width  = this.renderer.width;
+		let height = this.renderer.height;
+
+		for (let p = 0, pl = planes.length; p < pl; p++) {
+
+			let plane = planes[p];
+			if (plane.position.y < -1 / 2 * height) {
+				plane.position.x = -1 / 2 * width + plane.width;
+				plane.position.y = 0;
+			}
+
+		}
+
+		// TODO: Reset all planes
+	};
+
+
+	/*
+	 * IMPLEMENTATION
+	 */
+
+	let Composite = function(main) {
+
+		_State.call(this, main);
+
+
+		this.twister = new _Mersenne(1337);
+
+
+		this.__cache = {
+			background: null,
+			goals:      [],
+			info:       null,
+			planes:     []
+		};
+
+		this.__statistics = {
+			generation: 1,
+			score:      0,
+			highscore:  0
+		};
+
+
+		this.deserialize(_BLOB);
+
+
+
+		/*
+		 * INITIALIZATION
+		 */
+
+		let viewport = this.viewport;
+		if (viewport !== null) {
+
+			viewport.bind('reshape', function(orientation, rotation) {
+
+				let renderer = this.renderer;
+				if (renderer !== null) {
+
+					let entity = null;
+					let width  = renderer.width;
+					let height = renderer.height;
+
+
+					entity = this.queryLayer('bg', 'background');
+					entity.trigger('reshape', [ null, null, width, height ]);
+					this.__cache.background = entity;
+
+					entity = this.queryLayer('ui', 'info');
+					entity.setPosition({
+						y: -1 / 2 * height + 32
+					});
+					this.__cache.info = entity;
+
+				}
+
+			}, this);
+
+		}
+
+	};
+
+
+	Composite.prototype = {
+
+		/*
+		 * ENTITY API
+		 */
+
+		serialize: function() {
+
+			let data = _State.prototype.serialize.call(this);
+			data['constructor'] = 'game.state.Game';
+
+
+			return data;
+
+		},
+
+		deserialize: function(blob) {
+
+			_State.prototype.deserialize.call(this, blob);
+
+
+			let renderer = this.renderer;
+			let layer    = this.getLayer('ai');
+
+			if (renderer !== null && layer !== null) {
+
+				layer = this.getLayer('game');
+
+				for (let g = 0; g < (renderer.width / 256); g++) {
+					let goal = new _Goal();
+					this.__cache.goals.push(goal);
+					layer.addEntity(goal);
+				}
+
+				_reset_goals.call(this);
+
+
+				for (let p = 0; p < 64; p++) {
+					let plane = new _Plane();
+					this.__cache.planes.push(plane);
+					layer.addEntity(plane);
+				}
+
+				_reset_planes.call(this);
+
+
+				// TODO: ENN AI Layer
+
+			}
+
+		},
+
+
+
+		/*
+		 * CUSTOM API
+		 */
+
+		enter: function(oncomplete) {
+
+			_reset_goals.call(this);
+			_reset_planes.call(this);
+
+
+			let input = new lychee.Input({
+				touch: true
+			});
+
+			input.bind('touch', function() {
+
+				let plane = this.__cache.planes[0] || null;
+				if (plane !== null) {
+					plane.flap();
+				}
+
+			}, this);
+
+
+			if (oncomplete !== null) {
+				oncomplete(true);
+			}
+
+		},
+
+		update: function(clock, delta) {
+
+			_State.prototype.update.call(this, clock, delta);
+
+
+			let cache  = this.__cache;
+			let width  = this.renderer.width;
+			let height = this.renderer.height;
+
+
+			let background = cache.background;
+			if (background !== null) {
+
+				let origin = background.origin.x - (delta / 1000) * 128;
+				background.origin.x  = (origin % background.width) | 0;
+				background.__isDirty = true;
+
+			}
+
+
+			let goals  = cache.goals;
+			let planes = cache.planes;
+
+			for (let g = 0, gl = goals.length; g < gl; g++) {
+
+				let goal = goals[g];
+
+				for (let p = 0, pl = planes.length; p < pl; p++) {
+
+					let plane = planes[p];
+
+					if (plane.alive === true && goal.collidesWith(plane) === true) {
+
+						if (plane.effects.length === 0) {
+							plane.addEffect(new _Explosion({
+								position: plane.position
+							}));
+						}
+
+						plane.alive = false;
+
+					}
+
+					if (plane.alive === true) {
+
+						// TODO: AI interaction
+
+
+						if (plane.position.y > 1 / 2 * height) {
+
+							if (plane.effects.length === 0) {
+								plane.addEffect(new _Explosion({
+									position: plane.position
+								}));
+							}
+
+							plane.alive = false;
+
+						}
+
+					}
+
+				}
+
+
+				// TODO: Collision detection
+				// TODO: Plane interaction
+
+				if (goal.position.x < -1 / 2 * width) {
+					_reset_goals.call(this);
+				}
+
+			}
+
+		}
+
+	};
+
+
+	return Composite;
+
+});
